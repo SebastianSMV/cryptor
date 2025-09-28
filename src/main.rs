eacbb2af87717aa68a21e638f9fd4c5985a4cf6b
SSMV <granholmsebastian@proton.me>
2025-09-28 16:45:24 +0200

    Initial push

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use encryptor;
 use decryptor;
 
 fn main() {
-    let input: String = String::from("Hello 12039819878//(/(31ii0d9saiodjhuic8721y7m na 87ya87dn 2138721 y7op1ij34i12o7sy217");
+    let input: String = String::from("Simple encryption program!");
     let enc = encryptor::encrypt(&input, 92871227);
     println!("Input: {}\n",input);
     println!("Input encoded: {}\n",&enc);
